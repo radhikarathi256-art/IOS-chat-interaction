0e5d0cce98d406c2c6617d09087e4795cb6302b5
Radhika Rathi <radhikarathi256@gmail.com>
2026-08-20 14:54:55 +0530

    Initial Commit

---
+++ AstroChat/AstroChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroChatApp.swift
+//  AstroChat
+//
+//  Created by Radhika Rathi on 20/08/26.
+//
+
+import SwiftUI
+
+@main
+struct AstroChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstroChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AstroChat
+//
+//  Created by Radhika Rathi on 20/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
